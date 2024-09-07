92430ead7793abd3773858479ca0578bddf0d137
Alfarooq Almenghawi <a.almenghawi123@gmail.com>
2024-09-07 14:08:51 +0100

    this is the sprint of day 5 of intro week.

---
+++ test.js
@@ -0,0 +1,21 @@
+const review = `Northcoders: Week 1 Review (Phase: Intro Week) ✨
+
+First week of my Northcoders JavaScript Bootcamp, also known as Intro Week, was an almost smooth and engaging start to this journey. While it was designed to ease us into the course, I know that the real challenges are just around the corner—and I’m excited (and scared) for them.I also feel like the intro week wasn’t fast-paced compared to what may be coming. But if it is, that probably means I’ve already adjusted to the fast-paced nature of this bootcamp, which is a good sign.
+
+Here's a quick reflection on what I've loved about this week so far:
+
+1. Pair Programming: One of the highlights this week was being introduced to pair programming from day 1. It's a collaborative 1-1 approach that not only made coding less frustrating but also gave me a taste of what’s common in the industry. Solving problems together has been a great way to learn from others and stay motivated.
+
+2. The Community: The Northcoders staff and my fellow students have created an incredibly positive and supportive environment. I’ve been surrounded by people who are not only ambitious and intelligent but also genuinely caring, patient, and always ready to help. It’s inspiring to be part of such a diverse and uplifting group.
+
+Here's what was covered this week:
+
+Day 1: Git and Github
+Day 2: module.exports and js.require (these allow for variables and functions to run across different js files)
+Day 3: Problem Solving
+Day 4: Errors and Debugging
+Day 5: Intro to HTML and CSS
+
+#Northcoders #Bootcamp #Coding`
+
+console.log(review)
